23b055946ab8519c923222980c2897522a32aa56
Dominik Filipiak <df@aiclearing.com>
2021-10-27 11:02:11 +0200

    ResNet50 & ResNet101 training scripts

---
+++ scripts/run-baseline-resnet50-sup5.sh
@@ -0,0 +1,10 @@
+CENTERMASK_HOME="/home/ptempczyk/df/centermask2"
+CONDA_HOME="/home/ptempczyk/anaconda3"
+
+source $CONDA_HOME/bin/activate centermask2
+
+python $CENTERMASK_HOME/train_net.py \
+  --num-gpus 8 \
+  --config $CENTERMASK_HOME/configs/centermask-semisup/baseline_centermask_R_50_FPN_ms_3x_sup5.yaml
+
+source $CONDA_HOME/bin/deactivate
