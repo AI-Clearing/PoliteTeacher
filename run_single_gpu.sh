4043f10065ce3d6713ff60d82204459bd84a9584
azapala <zapala333@gmail.com>
2022-05-08 15:07:03 +0200

    update memory size

--- run_single_gpu.sh
+++ run_single_gpu.sh
@@ -1,3 +1,3 @@
 source /home/azapala/anaconda3/bin/activate detectron2
 cd ~/ssis/
-srun  --qos=8gpu7d --time 3-0 --gres=gpu:1 --mem-per-cpu 3000 --constraint=homedir  python train_net.py "$@"
+srun  --qos=8gpu7d --time 3-0 --gres=gpu:1 --mem-per-cpu=5000 --constraint=homedir  python train_net.py "$@"
