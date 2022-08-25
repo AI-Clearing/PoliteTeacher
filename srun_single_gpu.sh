24a8ccbd1c1f30cd967f4b0eb1eebf7a9ca5eb49
Dominik Filipiak <757111+DominikFilipiak@users.noreply.github.com>
2022-08-25 14:47:46 +0200

    Proper running script adjusted for new entropy

--- srun_single_gpu.sh
+++ srun_single_gpu.sh
@@ -1,3 +1,3 @@
 source /home/azapala/anaconda3/bin/activate detectron2
 cd ~/ssis/
-srun  --partition=common  --qos=8gpu7d --time=3-0 --gres=gpu:"$1" --mem-per-cpu=5000 --constraint=homedir  python train_net.py --num-gpus="$1" "${@:2}"
+srun  --partition=common  --qos=16gpu7d --time=3-0 --gres=gpu:"$1" --mem-per-gpu=12G python train_net.py --num-gpus="$1" "${@:2}"
