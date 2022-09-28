4a78381d41dff497988ae180f6df517a9fb7ab2d
Dominik Filipiak <757111+DominikFilipiak@users.noreply.github.com>
2022-09-28 23:49:35 +0200

    Further anonymisation

--- srun_single_gpu.sh
+++
@@ -1,3 +0,0 @@
-source /home/azapala/anaconda3/bin/activate detectron2
-cd ~/ssis/
-srun  --partition=common  --qos=16gpu7d --time=3-0 --gres=gpu:"$1" --mem-per-gpu=12G python train_net.py --num-gpus="$1" "${@:2}"
